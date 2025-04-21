413487bff2a86d59d393aa454ff6bc0dc762099f
kvaszar <157027054+kvaszar@users.noreply.github.com>
2025-04-21 15:32:22 +0300

    mostly done with t5

--- scripts/bastion.js
+++ scripts/bastion.js
@@ -1,15 +1,9 @@
 const bastion = extend(UnitType, "bastion", {
-    spawn(team, x, y, rotation, cons) {
-        print("hello");
-        print(this);
-        print(team);
-        this.super$spawn(team, x, y, rotation, cons);
-    },
     update(unit) {
+        this.super$update(unit);
         if (unit.isShooting) {
             unit.vel.scl(0);
         }
-        this.super$update(unit);
     },
 });
 bastion.constructor = () => extend(LegsUnit, {});
